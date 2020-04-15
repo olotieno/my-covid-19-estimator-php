8fdff6c62f630fed45045b451082ede20a2a9a6a
Oliver Lando <oliverlandootieno@gmail.com>
2020-04-15 09:56:32 +0300

    Updates

--- src/json.php
+++ src/json.php
@@ -14,6 +14,6 @@ function print_json($data)
 $time2 = microtime(true);
 
 $exe_time = "0".(int)($time2- $_SERVER["REQUEST_TIME_FLOAT"])* 1000;
-
+//$exe_time = "0".(int)($time2- $time1)* 1000;
 $logMessage = $_SERVER['REQUEST_METHOD']. "\t\t".$_SERVER['REQUEST_URI']. "\t\t".http_response_code()."\t\t". $exe_time."ms";
 file_put_contents('logs.txt', $logMessage."\n", FILE_APPEND | LOCK_EX);
