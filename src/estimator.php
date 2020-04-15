35c255b056419f7abb2d6d787febf930d48ac126
Oliver Lando <oliverlandootieno@gmail.com>
2020-04-15 09:33:23 +0300

    theChallenges

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,6 @@
 <?php
 $time1 = microtime(true);
-// required headers
+
 header("Access-Control-Allow-Origin: *");
 //header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
